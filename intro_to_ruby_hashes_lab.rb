e8db54aac1e678161a3619498fd6a52030cef94f
Rochelle Bondoc <rochelleannebaes@gmail.com>
2020-02-20 22:47:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,8 +24,10 @@ end
 
 def update_counting_hash(hash, key)
   if hash[key]
-    return hash = hash[key] += 1
+    hash[key] += 1
+    return hash
   else
-    return hash = hash[key] = 1
+    hash[key] = 1
+    return hash
    end
 end
